cb552cf4499ca39cf113b040099d351e4bf4fecc
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-06-25 20:44:19 +0100

    Add App Assets file

---
+++ lib/core/Utils/app_assets.dart
@@ -0,0 +1,5 @@
+abstract class Assets {
+  static const String logo = 'assets/images/splash_logo.svg';
+  static const String splash_upper_plant = 'assets/images/splash_upper_plant.svg';
+  static const String splash_bottom_section = 'assets/images/splash_bottom_section.svg';
+}
