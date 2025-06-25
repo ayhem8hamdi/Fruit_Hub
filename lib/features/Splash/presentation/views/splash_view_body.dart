40eb5fafa64702af7b51ab6b113528966c3e46e7
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-06-25 21:57:37 +0100

    Add SafeArea to Splash Screen

--- lib/features/Splash/presentation/views/splash_view_body.dart
+++ lib/features/Splash/presentation/views/splash_view_body.dart
@@ -7,25 +7,24 @@ class SplashViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SvgPicture.asset(Assets.splash_upper_plant),
-          ],
-        ),
-        SvgPicture.asset(Assets.logo),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 5, right: 5, left: 5),
-          child: SvgPicture.asset(
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SvgPicture.asset(Assets.splash_upper_plant),
+            ],
+          ),
+          SvgPicture.asset(Assets.logo),
+          SvgPicture.asset(
             Assets.splash_bottom_section,
             fit: BoxFit.fill,
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
