0d80028a0f36850ebd089b085605490310b29648
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-06-26 02:07:12 +0100

    Done Splash Feature Logic

--- lib/core/Utils/app_router.dart
+++ lib/core/Utils/app_router.dart
@@ -1,14 +1,19 @@
+import 'package:advanced_ecommerce/features/OnBoarding/presentation/views/on_boarding_view.dart';
 import 'package:advanced_ecommerce/features/Splash/presentation/views/splash_view.dart';
 import 'package:get/get.dart';
 
 abstract class AppRouter {
   static String splashScreen = "/";
-
+  static String onBoardingScreen = "/onBoardingScreen";
   static List<GetPage<dynamic>>? getViews() {
     return [
       GetPage(
         name: splashScreen,
         page: () => const SplashView(),
+      ),
+      GetPage(
+        name: onBoardingScreen,
+        page: () => const OnBoardingView(),
       ),
     ];
   }
