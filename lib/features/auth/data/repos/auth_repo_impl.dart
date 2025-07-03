ab27ef96b0fa801de2f8062330437e7fb42cf1f9
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-07-03 01:58:19 +0100

    Done AuthRepo create user method

--- lib/features/auth/data/repos/auth_repo_impl.dart
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -1,1 +1,41 @@
+import 'package:advanced_ecommerce/core/errors/failure.dart';
+import 'package:advanced_ecommerce/core/services/firebase_auth_service.dart';
+import 'package:advanced_ecommerce/features/auth/data/models/user_model.dart';
+import 'package:advanced_ecommerce/features/auth/domain/entities/user_entity.dart';
+import 'package:advanced_ecommerce/features/auth/domain/repos/auth_repo.dart';
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
+class AuthRepoImpl implements AuthRepo {
+  final FireBaseAuthService fireBaseAuthService;
+
+  AuthRepoImpl(this.fireBaseAuthService);
+
+  @override
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+    required String fullName,
+    required String phoneNumber,
+  }) async {
+    try {
+      final User firebaseUser = await fireBaseAuthService.registerUser(
+        email: email,
+        password: password,
+        fullName: fullName,
+        phoneNumber: phoneNumber,
+      );
+
+      final UserModel userModel = UserModel.fromFirebaseUser(
+        firebaseUser: firebaseUser,
+        phoneNumber: phoneNumber,
+        userName: fullName,
+      );
+
+      return Right(userModel);
+    } catch (error) {
+      final failure = Failure.fromException(error);
+      return Left(failure);
+    }
+  }
+}
