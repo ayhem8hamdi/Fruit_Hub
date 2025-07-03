69db2a4d79467829ac297f93397138c1bf129b46
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-07-03 02:19:58 +0100

    Create SignUp Cubit method

--- lib/features/auth/presentation/manager/signup_cubit/cubit/sign_up_cubit.dart
+++ lib/features/auth/presentation/manager/signup_cubit/cubit/sign_up_cubit.dart
@@ -1,8 +1,30 @@
+import 'package:advanced_ecommerce/core/errors/failure.dart';
+import 'package:advanced_ecommerce/features/auth/data/repos/auth_repo_impl.dart';
+import 'package:advanced_ecommerce/features/auth/domain/entities/user_entity.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'sign_up_state.dart';
 
 class SignUpCubit extends Cubit<SignUpState> {
-  SignUpCubit() : super(SignUpInitial());
+  SignUpCubit(this.authRepoImpl) : super(SignUpInitial());
+  final AuthRepoImpl authRepoImpl;
+
+  Future<void> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+    required String fullName,
+    required String phoneNumber,
+  }) async {
+    emit(SignUpLoading());
+    final result = await authRepoImpl.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+        fullName: fullName,
+        phoneNumber: phoneNumber);
+    result.fold(
+      (failure) => emit(SignUpFailed(failure: failure)),
+      (result) => emit(SignUpSucces(userEntity: result)),
+    );
+  }
 }
