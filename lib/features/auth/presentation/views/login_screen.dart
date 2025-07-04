c1d07b35df90f884d32a680f0b5e6d9d0072590b
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-07-04 20:54:11 +0100

    Create login Body Bloc Consumer

--- lib/features/auth/presentation/views/login_screen.dart
+++ lib/features/auth/presentation/views/login_screen.dart
@@ -1,8 +1,7 @@
 import 'package:advanced_ecommerce/core/services/get_it_service.dart';
 import 'package:advanced_ecommerce/features/auth/domain/repos/auth_repo.dart';
-import 'package:advanced_ecommerce/features/auth/presentation/manager/login_cubit/cubit/login_cubit_cubit.dart';
 import 'package:advanced_ecommerce/features/auth/presentation/manager/social_media_auth_cubit/cubit/social_media_auth_cubit.dart';
-import 'package:advanced_ecommerce/features/auth/presentation/views/widgets/login_screen_body.dart';
+import 'package:advanced_ecommerce/features/auth/presentation/views/widgets/login_body_bloc_listener.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -13,7 +12,7 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => SocialMediaAuthCubit(getIt.get<AuthRepo>()),
-      child: const LoginScreenBody(),
+      child: const LoginBodyBlocListener(),
     );
   }
 }
