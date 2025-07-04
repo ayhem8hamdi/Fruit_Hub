81424fa3e1f72fe949fd1ddafaecb6d2ca1c1b43
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-07-04 03:51:31 +0100

    Create  Social Media Auth Cubit Method

--- lib/features/auth/presentation/manager/social_media_auth_cubit/cubit/social_media_auth_cubit.dart
+++ lib/features/auth/presentation/manager/social_media_auth_cubit/cubit/social_media_auth_cubit.dart
@@ -1,8 +1,22 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:advanced_ecommerce/core/errors/failure.dart';
+import 'package:advanced_ecommerce/features/auth/domain/entities/user_entity.dart';
+import 'package:advanced_ecommerce/features/auth/domain/repos/auth_repo.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'social_media_auth_state.dart';
 
 class SocialMediaAuthCubit extends Cubit<SocialMediaAuthState> {
-  SocialMediaAuthCubit() : super(SocialMediaAuthInitial());
+  SocialMediaAuthCubit(this.authRepo) : super(SocialMediaAuthInitial());
+  final AuthRepo authRepo;
+  Future<void> loginUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    emit(SocialMediaAuthLoading());
+    final result = await authRepo.loginWithGoogle();
+    result.fold(
+      (failure) => emit(SocialMediaAuthFailure(failure: failure)),
+      (result) => emit(SocialMediaAuthSucces(userEntity: result)),
+    );
+  }
 }
