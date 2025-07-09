1d5f8b983906c8356386d8c28faca621626704a0
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-07-09 17:00:38 +0100

    Add startPhoneVerification method to PasswordForgotFirebaseService class

--- lib/core/services/password_forgot_firebase_service.dart
+++ lib/core/services/password_forgot_firebase_service.dart
@@ -1,15 +1,18 @@
+import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class PasswordForgotFirebaseService {
   final FirebaseFirestore _firestore;
+  final FirebaseAuth _auth;
 
-  PasswordForgotFirebaseService(this._firestore);
+  PasswordForgotFirebaseService(this._firestore, this._auth);
 
+  /// Checks if phone exists in Firestore users collection
   Future<bool> formatAndVerifyPhoneNumber(String userInputPhone) async {
     final formattedPhone =
         '+216${userInputPhone.replaceAll(RegExp(r'[^\d]'), '')}';
 
-    // Check Firestore for existence
     final query = await _firestore
         .collection('users')
         .where('phone', isEqualTo: formattedPhone)
@@ -17,5 +20,23 @@ class PasswordForgotFirebaseService {
         .get();
 
     return query.docs.isNotEmpty;
+  }
+
+  /// Sends verification SMS code to given E.164 phone number, returns verificationId
+  Future<String> sendVerificationCode(String userInputPhone) async {
+    final completer = Completer<String>();
+
+    await _auth.verifyPhoneNumber(
+      phoneNumber: userInputPhone,
+      timeout: const Duration(seconds: 60),
+      verificationCompleted: (_) {
+        // No auto sign-in
+      },
+      verificationFailed: (e) => completer.completeError(e),
+      codeSent: (verificationId, _) => completer.complete(verificationId),
+      codeAutoRetrievalTimeout: (_) {},
+    );
+
+    return completer.future;
   }
 }
