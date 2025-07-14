b3c1c83f930486760e8b25adfb4e8cc68d1736c6
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-07-14 16:19:34 +0100

    Create SoftLeftCurveClipper Class (paint customized clipper)

--- lib/features/home/presentation/view/widgets/home_screen_body.dart
+++ lib/features/home/presentation/view/widgets/home_screen_body.dart
@@ -13,8 +13,24 @@ class HomeScreenBody extends StatelessWidget {
         HomeScreenUserListTile(),
         Gap(16),
         CustomSearchTextField(hintText: 'ابحث عن.......'),
-        Gap(12)
+        Gap(12),
       ],
     );
   }
+}
+
+class SoftLeftCurveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    path.moveTo(17, 0);
+    path.quadraticBezierTo(0, size.height / 2, 17, size.height);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
