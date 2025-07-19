f4d9ad3f923d867d0adba6daa4556111ccf46993
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-07-19 17:05:03 +0100

    Code refactor

--- lib/features/user_profile/presentation/views/widgets/menu_item_list_view_builder.dart
+++ lib/features/user_profile/presentation/views/widgets/menu_item_list_view_builder.dart
@@ -1,8 +1,7 @@
 import 'package:advanced_ecommerce/core/Utils/app_assets.dart';
 import 'package:advanced_ecommerce/features/user_profile/data/models/user_profile_parameter_menu.dart';
-import 'package:advanced_ecommerce/features/user_profile/presentation/views/widgets/custom_menu_text.dart';
+import 'package:advanced_ecommerce/features/user_profile/presentation/views/widgets/user_profile_parameter_menu_item.dart';
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
 
 class MenuItemListViewBuilder extends StatelessWidget {
   const MenuItemListViewBuilder({super.key});
@@ -45,30 +44,7 @@ class MenuItemListViewBuilder extends StatelessWidget {
         childCount: items.length,
         (context, index) {
           final item = items[index];
-          return GestureDetector(
-            onTap: item.onTap,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 22),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Image.asset(item.image),
-                      const Gap(7),
-                      CustomMenuText(text: item.title),
-                      const Spacer(),
-                      Image.asset(Assets.leftDirectedBackArrowIcon, width: 14),
-                    ],
-                  ),
-                  const Gap(5),
-                  const Divider(
-                    thickness: 1.4,
-                    color: Color(0xFFF2F3F3),
-                  )
-                ],
-              ),
-            ),
-          );
+          return MenuItem(item: item);
         },
       ),
     );
--- lib/features/user_profile/presentation/views/widgets/user_profile_parameter_menu_item.dart
+++ lib/features/user_profile/presentation/views/widgets/user_profile_parameter_menu_item.dart
@@ -1,37 +1,40 @@
 import 'package:advanced_ecommerce/core/Utils/app_assets.dart';
+import 'package:advanced_ecommerce/features/user_profile/data/models/user_profile_parameter_menu.dart';
 import 'package:advanced_ecommerce/features/user_profile/presentation/views/widgets/custom_menu_text.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
-class UserProfileParameterMenuItem extends StatelessWidget {
-  const UserProfileParameterMenuItem({super.key});
+class MenuItem extends StatelessWidget {
+  const MenuItem({
+    super.key,
+    required this.item,
+  });
+
+  final UserProfileParameterMenu item;
 
   @override
   Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
+    return GestureDetector(
+      onTap: item.onTap,
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 22),
         child: Column(
           children: [
             Row(
-              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Image.asset(
-                  Assets.boxIcon,
-                ),
+                Image.asset(item.image),
                 const Gap(7),
-                const CustomMenuText(
-                  text: 'الملف الشخصي',
-                ),
+                CustomMenuText(text: item.title),
                 const Spacer(),
-                Image.asset(Assets.leftDirectedBackArrowIcon)
+                Image.asset(Assets.leftDirectedBackArrowIcon, width: 14),
               ],
             ),
             const Gap(5),
             const Divider(
               thickness: 1.4,
               color: Color(0xFFF2F3F3),
-            )
+            ),
+            const Gap(4)
           ],
         ),
       ),
