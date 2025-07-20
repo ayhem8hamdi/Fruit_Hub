c35b941c87d576b11c66756a39969fa0969d8fb0
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-07-20 18:23:54 +0100

    Done Menu Item With Text Widget

--- lib/features/user_profile/presentation/views/widgets/menu_item_widget.dart
+++ lib/features/user_profile/presentation/views/widgets/menu_item_widget.dart
@@ -1,4 +1,5 @@
 import 'package:advanced_ecommerce/core/Utils/app_assets.dart';
+import 'package:advanced_ecommerce/core/Utils/app_styles.dart';
 import 'package:advanced_ecommerce/features/user_profile/data/models/user_profile_parameter_menu.dart';
 import 'package:advanced_ecommerce/features/user_profile/presentation/views/widgets/custom_menu_text.dart';
 import 'package:advanced_ecommerce/features/user_profile/presentation/views/widgets/switch_toggle_button.dart';
@@ -77,6 +78,49 @@ class MenuItemWithToggleButton extends StatelessWidget {
               const Gap(4)
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class MenuItemWithTextWidget extends StatelessWidget {
+  const MenuItemWithTextWidget({
+    super.key,
+    required this.item,
+  });
+
+  final UserProfileParameterMenu item;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: item.onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 22),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Image.asset(item.image),
+                const Gap(7),
+                CustomMenuText(text: item.title),
+                const Spacer(),
+                Text(
+                  'العربية',
+                  style: AppStyles.styleRegular13(context)
+                      .copyWith(color: Colors.black),
+                ),
+                Image.asset(Assets.leftDirectedBackArrowIcon, width: 14),
+              ],
+            ),
+            const Gap(5),
+            const Divider(
+              thickness: 1.4,
+              color: Color(0xFFF2F3F3),
+            ),
+            const Gap(4)
+          ],
         ),
       ),
     );
