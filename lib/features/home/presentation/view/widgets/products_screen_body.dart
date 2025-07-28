fb670e59c49d7ca765e44294df4ed0ab9474912d
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-07-28 19:16:32 +0100

    Done Products Screen Body

--- lib/features/home/presentation/view/widgets/products_screen_body.dart
+++ lib/features/home/presentation/view/widgets/products_screen_body.dart
@@ -2,6 +2,7 @@ import 'package:advanced_ecommerce/core/Utils/app_assets.dart';
 import 'package:advanced_ecommerce/features/home/presentation/view/widgets/best_seller_titles_line.dart';
 import 'package:advanced_ecommerce/features/home/presentation/view/widgets/custom_most_popular_product_appbar.dart';
 import 'package:advanced_ecommerce/features/home/presentation/view/widgets/custom_search_text_field.dart';
+import 'package:advanced_ecommerce/features/home/presentation/view/widgets/fruit_item_grid_delegate_builder.dart';
 import 'package:advanced_ecommerce/features/home/presentation/view/widgets/products_and_sorting_line.dart';
 import 'package:advanced_ecommerce/features/home/presentation/view/widgets/products_horiz_list.dart';
 import 'package:flutter/material.dart';
@@ -29,6 +30,7 @@ class ProductsScreenBody extends StatelessWidget {
           SliverGap(26),
           BestSellerTitlesLine(),
           SliverGap(10),
+          FruitItemGridDelegateBuilder()
         ],
       ),
     );
