9446b640e43fa47d8a816579075ca5c37ae85aa4
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-07-30 22:17:25 +0100

    Create Search Screen Titles Line

--- lib/features/search/presentaion/views/widgets/search_screen_body.dart
+++ lib/features/search/presentaion/views/widgets/search_screen_body.dart
@@ -1,3 +1,4 @@
+import 'package:advanced_ecommerce/core/Utils/app_styles.dart';
 import 'package:advanced_ecommerce/features/home/presentation/view/widgets/active_custom_search_field.dart';
 import 'package:advanced_ecommerce/features/home/presentation/view/widgets/custom_most_popular_product_appbar.dart';
 import 'package:flutter/material.dart';
@@ -16,9 +17,37 @@ class SearchScreenBody extends StatelessWidget {
           ),
           SliverGap(24),
           ActiveCustomSearchTextField(hintText: 'ابحث عن.......'),
-          SliverGap(12),
+          SliverGap(26),
+          SearchScreenBodyLineTitles()
         ],
       ),
+    );
+  }
+}
+
+class SearchScreenBodyLineTitles extends StatelessWidget {
+  const SearchScreenBodyLineTitles({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'عمليات البحث الأخيرة',
+                style: AppStyles.styleBold13(context)
+                    .copyWith(color: Colors.black),
+              ),
+              Text(
+                'حذف الكل',
+                style: AppStyles.styleRegular13(context)
+                    .copyWith(color: const Color(0XFF949D9E)),
+              )
+            ],
+          )),
     );
   }
 }
