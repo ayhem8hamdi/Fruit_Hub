4ee478758ad0b14dc6154eaa9c8c75ac5db95587
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-07-31 02:35:05 +0100

    Done Personal Data Form Part 1

--- lib/features/user_profile/presentation/views/widgets/personal_data_screen_body.dart
+++ lib/features/user_profile/presentation/views/widgets/personal_data_screen_body.dart
@@ -1,3 +1,9 @@
+import 'dart:developer';
+
+import 'package:advanced_ecommerce/core/Utils/app_styles.dart';
+import 'package:advanced_ecommerce/core/Utils/methods_helper.dart';
+import 'package:advanced_ecommerce/features/OnBoarding/presentation/views/widgets/on_boarding_button.dart';
+import 'package:advanced_ecommerce/features/auth/presentation/views/widgets/custom_text_field.dart';
 import 'package:advanced_ecommerce/features/card_and_products_details/presentation/views/widgets/custom_card_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
@@ -14,6 +20,69 @@ class PersonalDataScreenBody extends StatelessWidget {
             title: 'الملف الشخصي',
           ),
           SliverGap(24),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: PersonalDataForm(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class PersonalDataForm extends StatefulWidget {
+  const PersonalDataForm({super.key});
+
+  @override
+  State<PersonalDataForm> createState() => _PersonalDataFormState();
+}
+
+class _PersonalDataFormState extends State<PersonalDataForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void onLoginPressed() {
+    if (_formKey.currentState?.validate() ?? false) {
+      final email = _emailController.text.trim();
+      final password = _passwordController.text.trim();
+      log('$email $password');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            'المعلومات الشخصيه',
+            style: AppStyles.styleSemiBold13(context)
+                .copyWith(color: Colors.black),
+          ),
+          const Gap(8),
+          CustomBorderTextField(
+              hintText: 'أحمد ياسر',
+              controller: _emailController,
+              validator: MethodsHelper.validateEmail,
+              isItInProfileData: true),
+          const Gap(8),
+          CustomBorderTextField(
+              hintText: 'ahmedyacer@gmail.com',
+              controller: _emailController,
+              validator: MethodsHelper.validateEmail,
+              isItInProfileData: true),
         ],
       ),
     );
