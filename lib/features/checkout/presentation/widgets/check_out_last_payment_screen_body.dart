f5e73a66c62e73edf0d6f03f45307e9f07aa326f
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-08-07 02:26:06 +0100

    Create Confirm Payment Method Item Part One

--- lib/features/checkout/presentation/widgets/check_out_last_payment_screen_body.dart
+++ lib/features/checkout/presentation/widgets/check_out_last_payment_screen_body.dart
@@ -1,8 +1,11 @@
+import 'package:advanced_ecommerce/core/Utils/app_assets.dart';
+import 'package:advanced_ecommerce/core/Utils/app_styles.dart';
 import 'package:advanced_ecommerce/features/card_and_products_details/presentation/views/widgets/custom_card_appbar.dart';
 import 'package:advanced_ecommerce/features/checkout/presentation/widgets/check_out_step_row.dart';
 import 'package:advanced_ecommerce/features/checkout/presentation/widgets/order_resume_item.dart';
 import 'package:advanced_ecommerce/features/checkout/presentation/widgets/order_resume_title.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
 
 class CheckoutLastPaymentScreenBody extends StatelessWidget {
@@ -20,10 +23,61 @@ class CheckoutLastPaymentScreenBody extends StatelessWidget {
           stepNumber: 3,
         ),
         SliverGap(24),
-        OrderResumeTitle(),
+        OrderResumeTitle(
+          title: 'ملخص الطلب :',
+        ),
         SliverGap(8),
-        OrderResumeItem()
+        OrderResumeItem(),
+        SliverGap(16),
+        OrderResumeTitle(title: 'يرجي تأكيد  طلبك'),
+        SliverGap(8),
+        ConfirmPaymentMethodItem()
       ],
+    );
+  }
+}
+
+class ConfirmPaymentMethodItem extends StatelessWidget {
+  const ConfirmPaymentMethodItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 9),
+        margin: const EdgeInsets.symmetric(horizontal: 19),
+        decoration: BoxDecoration(
+            color: const Color(0XFFF2F3F3).withOpacity(0.4),
+            borderRadius: BorderRadius.circular(4)),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  'وسيلة الدفع',
+                  style: AppStyles.styleBold13(context)
+                      .copyWith(color: Colors.black),
+                ),
+                Spacer(),
+                Row(
+                  children: [
+                    SvgPicture.asset(
+                      Assets.editIcon,
+                      color: const Color(0XFF6C7275),
+                    ),
+                    Gap(4),
+                    Text(
+                      'تعديل',
+                      style: AppStyles.styleSemiBold13(context)
+                          .copyWith(color: const Color(0XFF949D9E)),
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/features/checkout/presentation/widgets/order_resume_title.dart
+++ lib/features/checkout/presentation/widgets/order_resume_title.dart
@@ -2,15 +2,15 @@ import 'package:advanced_ecommerce/core/Utils/app_styles.dart';
 import 'package:flutter/material.dart';
 
 class OrderResumeTitle extends StatelessWidget {
-  const OrderResumeTitle({super.key});
-
+  const OrderResumeTitle({super.key, required this.title});
+  final String title;
   @override
   Widget build(BuildContext context) {
     return SliverToBoxAdapter(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Text(
-          'ملخص الطلب :',
+          title,
           style: AppStyles.styleBold13(context)
               .copyWith(color: const Color(0XFF0C0D0D)),
         ),
