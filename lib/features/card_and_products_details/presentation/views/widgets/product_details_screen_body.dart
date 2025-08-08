64e4d0fe18e31f3a9b4663f883ecd28fece004fa
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-08-08 21:54:55 +0100

    Done Products Feature

--- lib/features/card_and_products_details/presentation/views/widgets/product_details_screen_body.dart
+++ lib/features/card_and_products_details/presentation/views/widgets/product_details_screen_body.dart
@@ -1,4 +1,5 @@
 import 'package:advanced_ecommerce/features/card_and_products_details/presentation/views/widgets/product_description_text.dart';
+import 'package:advanced_ecommerce/features/card_and_products_details/presentation/views/widgets/product_features.dart';
 import 'package:advanced_ecommerce/features/card_and_products_details/presentation/views/widgets/product_image_and_upper_section.dart';
 import 'package:advanced_ecommerce/features/card_and_products_details/presentation/views/widgets/product_quantity_and_pricing_row.dart';
 import 'package:advanced_ecommerce/features/card_and_products_details/presentation/views/widgets/rating_and_comments_link_row.dart';
@@ -20,6 +21,7 @@ class ProductDetailsScreenBody extends StatelessWidget {
         SliverGap(9),
         ProductDescriptionText(),
         SliverGap(17),
+        ProductFeatures()
       ],
     );
   }
---
+++ lib/features/card_and_products_details/presentation/views/widgets/product_feature_items.dart
@@ -0,0 +1,45 @@
+import 'package:advanced_ecommerce/core/Utils/app_styles.dart';
+import 'package:advanced_ecommerce/features/card_and_products_details/data/product_feature_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
+
+class ProductFeatureItem extends StatelessWidget {
+  const ProductFeatureItem({super.key, required this.productFeatureModel});
+  final ProductFeatureModel productFeatureModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 12),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(color: const Color(0XFF979899).withOpacity(0.25)),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                productFeatureModel.value,
+                style: AppStyles.styleBold16(context)
+                    .copyWith(color: const Color(0XFF23AA49)),
+              ),
+              const Gap(8),
+              Text(
+                productFeatureModel.title,
+                style: AppStyles.styleSemiBold13(context)
+                    .copyWith(color: const Color(0XFF979899)),
+              )
+            ],
+          ),
+          const Gap(16),
+          SvgPicture.asset(productFeatureModel.icon)
+        ],
+      ),
+    );
+  }
+}
