64eddd8e9675ada0b2ea164b3053f938a8ced8cb
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-08-08 21:16:09 +0100

    Create Rating And Comments Link Row

--- lib/features/card_and_products_details/presentation/views/widgets/product_details_screen_body.dart
+++ lib/features/card_and_products_details/presentation/views/widgets/product_details_screen_body.dart
@@ -1,3 +1,5 @@
+import 'package:advanced_ecommerce/core/Utils/app_colors.dart';
+import 'package:advanced_ecommerce/core/Utils/app_styles.dart';
 import 'package:advanced_ecommerce/features/card_and_products_details/presentation/views/widgets/product_image_and_upper_section.dart';
 import 'package:advanced_ecommerce/features/card_and_products_details/presentation/views/widgets/product_quantity_and_pricing_row.dart';
 import 'package:flutter/material.dart';
@@ -12,8 +14,52 @@ class ProductDetailsScreenBody extends StatelessWidget {
       slivers: [
         ProductImageAndUpperSection(),
         SliverGap(24),
-        ProductQuantityAndPriceRow()
+        ProductQuantityAndPriceRow(),
+        SliverGap(9),
+        RatingAndCommentsLinkRow()
       ],
+    );
+  }
+}
+
+class RatingAndCommentsLinkRow extends StatelessWidget {
+  const RatingAndCommentsLinkRow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Row(
+          children: [
+            const Icon(
+              Icons.star,
+              color: Color(0XFFFFC529),
+            ),
+            const Gap(9),
+            Text(
+              '4.5',
+              style: AppStyles.styleSemiBold13(context).copyWith(
+                color: const Color(0XFF111719),
+              ),
+            ),
+            const Gap(9),
+            Text(
+              '(30+)',
+              style: AppStyles.styleRegular13(context)
+                  .copyWith(color: const Color(0XFF9796A1)),
+            ),
+            const Gap(9),
+            Text(
+              'المراجعه',
+              style: AppStyles.styleBold13(context).copyWith(
+                color: AppColors.kPrimaryColor,
+                decoration: TextDecoration.underline,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
