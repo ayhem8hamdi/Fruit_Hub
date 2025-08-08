13b3b3c0262fcc2d64479bf21f6198f73107c7c0
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-08-08 21:22:40 +0100

    Create Product Description Text

--- lib/features/card_and_products_details/presentation/views/widgets/product_details_screen_body.dart
+++ lib/features/card_and_products_details/presentation/views/widgets/product_details_screen_body.dart
@@ -1,7 +1,7 @@
-import 'package:advanced_ecommerce/core/Utils/app_colors.dart';
-import 'package:advanced_ecommerce/core/Utils/app_styles.dart';
+import 'package:advanced_ecommerce/features/card_and_products_details/presentation/views/widgets/product_description_text.dart';
 import 'package:advanced_ecommerce/features/card_and_products_details/presentation/views/widgets/product_image_and_upper_section.dart';
 import 'package:advanced_ecommerce/features/card_and_products_details/presentation/views/widgets/product_quantity_and_pricing_row.dart';
+import 'package:advanced_ecommerce/features/card_and_products_details/presentation/views/widgets/rating_and_comments_link_row.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -16,50 +16,11 @@ class ProductDetailsScreenBody extends StatelessWidget {
         SliverGap(24),
         ProductQuantityAndPriceRow(),
         SliverGap(9),
-        RatingAndCommentsLinkRow()
+        RatingAndCommentsLinkRow(),
+        SliverGap(9),
+        ProductDescriptionText(),
+        SliverGap(17),
       ],
-    );
-  }
-}
-
-class RatingAndCommentsLinkRow extends StatelessWidget {
-  const RatingAndCommentsLinkRow({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Row(
-          children: [
-            const Icon(
-              Icons.star,
-              color: Color(0XFFFFC529),
-            ),
-            const Gap(9),
-            Text(
-              '4.5',
-              style: AppStyles.styleSemiBold13(context).copyWith(
-                color: const Color(0XFF111719),
-              ),
-            ),
-            const Gap(9),
-            Text(
-              '(30+)',
-              style: AppStyles.styleRegular13(context)
-                  .copyWith(color: const Color(0XFF9796A1)),
-            ),
-            const Gap(9),
-            Text(
-              'المراجعه',
-              style: AppStyles.styleBold13(context).copyWith(
-                color: AppColors.kPrimaryColor,
-                decoration: TextDecoration.underline,
-              ),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
